7da73cf8cfbd7c40e3604940771165d778d4688a
Rodrigo Porto <rodrigo.porto@animati.com.br>
2024-05-06 13:37:04 -0300

    delete swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import "express-async-errors";
 import { errorHandler } from './middlewares/error-handler';
+import routes from './routes';
 
 
 const app = express();
 const port = 3333;
 app.use(express.json());
+app.use(routes);
 // app.use(cors());
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // app.use('/files', express.static(path.resolve(__dirname, '..', "tmp")));
